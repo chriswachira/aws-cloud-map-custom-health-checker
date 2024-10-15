89b899595506e4585d6f03a83870089742c16f6c
Chris Wachira <chriskane816@gmail.com>
2024-10-15 15:20:03 +0300

    feat: Add Dockerfile for containerization

--- services/servicediscovery.go
+++ services/servicediscovery.go
@@ -10,6 +10,30 @@ import (
 
 // const DEREGISTER_INSTANCE_MAX_RETRIES = 5
 
+// func UpdateInstanceCustomHealthStatus(client servicediscovery.Client, taskArn string, discoveryArn string, status types.CustomHealthStatus) {
+// 	taskARNSlice := strings.Split(taskArn, "/")
+// 	cloudMapServiceIdSlice := strings.Split(discoveryArn, "/")
+
+// 	taskId := taskARNSlice[len(taskARNSlice)-1]
+// 	cloudMapServiceId := cloudMapServiceIdSlice[len(cloudMapServiceIdSlice)-1]
+
+// 	log.Println("TaskID - ", taskId)
+// 	log.Println("Service ID - ", cloudMapServiceId)
+
+// 	var instanceCustomHealthStatusParams servicediscovery.UpdateInstanceCustomHealthStatusInput
+// 	instanceCustomHealthStatusParams.InstanceId = &taskId
+// 	instanceCustomHealthStatusParams.ServiceId = &cloudMapServiceId
+// 	instanceCustomHealthStatusParams.Status = status
+
+// 	_, err := client.UpdateInstanceCustomHealthStatus(context.TODO(), &instanceCustomHealthStatusParams)
+// 	if err != nil {
+// 		log.Fatal("There was an error updating the instance's health to Cloud Map service: ", err)
+// 	}
+
+// 	log.Printf("Successfully updated health status of instance %s to %s on %s!", taskId, string(status), cloudMapServiceId)
+
+// }
+
 func DeregisterTaskFromCloudMapService(client servicediscovery.Client, taskARN string, discoveryARN string) bool {
 
 	taskARNSlice := strings.Split(taskARN, "/")
